c6290193ef56db9d58f6dbb5d8c083a26cd7c08b
SM-Sclass <smclover376@gmail.com>
2025-03-19 01:02:08 +0530

    unnecessary non working things removed

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,8 @@
-
 import './App.css'
 import AppRouter from './Routes/index'
 import { BrowserRouter } from 'react-router-dom'
-import { ThemeProvider } from './components/theme-provider'
 
 function App() {
-
   return (
     <BrowserRouter>
       <AppRouter />
